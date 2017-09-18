7d7b2fcb5f17c60ea3569e4ae3c95d97202e1628
lx <377247294@qq.com>
2017-09-18 10:54:59 +0800

    目录结构

--- wy/src/components/Home.js
+++ wy/src/components/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import '../css/Home.css'
 export default class Home extends React.Component{
     render(){
         return(
             <div>
+            <div className="hz"></div>
             </div>
             );
     }
