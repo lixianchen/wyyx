8ff1c9441ad02bde8f4b199eaf4a1876eb110034
lx <377247294@qq.com>
2017-09-18 11:17:07 +0800

    目录结构

--- wy/src/components/Home.js
+++ wy/src/components/Home.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import '../css/Home.css'
+import { Input } from 'antd';
+const Search = Input.Search;
+
 export default class Home extends React.Component{
     render(){
         return(
             <div>
-            <div className="hz"></div>
+                <div className="box">
+                    <span>飞鱼逛街</span>
+                     <Search
+                        placeholder="请输入你想要是商品"
+                        style={{ width: 200 }}
+                        onSearch={value => console.log(value)}
+                      />
+
+                </div>
             </div>
             );
     }
